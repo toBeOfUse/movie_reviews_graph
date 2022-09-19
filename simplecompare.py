01fec6dbf3c5d707c0b41d43ff60f19d59620b6f
toBeOfUse <mitchjacov@gmail.com>
2022-09-19 17:47:20 -0400

    very slightly more informative data analyzer

--- simplecompare.py
+++ simplecompare.py
@@ -1,14 +1,25 @@
 import json
+from pathlib import Path
+from collections import defaultdict
 
-with open("review_data/manofsteel.json", encoding="utf-8") as steel_file:
-    steel_reviews = json.load(steel_file)
-steel_reviewers = set(review["userID"] for review in steel_reviews if review["rating"] != -1)
+reviewers: dict[str, int] = defaultdict(lambda: 0)
 
-with open("review_data/ironman.json", encoding="utf-8") as iron_file:
-    iron_reviews = json.load(iron_file)
-iron_reviewers = set(review["userID"] for review in iron_reviews if review["rating"] != -1)
+files = list(Path(".").glob("review_data/*.json"))
+for file_path in files:
+    with open(file_path, encoding="utf-8") as file:
+        reviews = json.load(file)
+    seen_reviewers = set()
+    for review in reviews:
+        if review["rating"] != -1 and review["userID"] not in seen_reviewers:
+            reviewers[review["userID"]] += 1
+            seen_reviewers.add(review["userID"])
 
-print(f"we have {len(steel_reviewers)} people who reviewed 'man of steel', "
-    f"{len(iron_reviewers)} people who reviewed 'iron man', "
-    f"and {len(steel_reviewers & iron_reviewers)} people who reviewed both."    
-)
+review_counts: dict[int, int] = defaultdict(lambda: 0)
+for review_count in reviewers.values():
+    assert review_count <= 3
+    review_counts[review_count] += 1
+
+
+print(f"in the files [{', '.join(map(str, files))}]:")
+for review_count, reviewers_with_that_count in review_counts.items():
+    print(reviewers_with_that_count, "people reviewed", review_count, "of the movies")
