e6888d06bd4cefa4eca5612b08d4472775f2a405
toBeOfUse <mitchjacov@gmail.com>
2022-10-31 11:55:31 -0400

    draw graph with colored edges

---
+++ analysis.py
@@ -0,0 +1,16 @@
+import matplotlib.pyplot as plt
+import networkx as nx
+from movienetwork import network
+
+def draw_graph():
+    edge_weights = [e[2] for e in network.edges.data("weight")]
+    min_weight, max_weight = min(edge_weights), max(edge_weights)
+    weight_range = max_weight-min_weight
+    normalize_weight = lambda e: (e-min_weight)/weight_range
+    colors = [(1-normalize_weight(e), normalize_weight(e), 0, 1) for e in edge_weights]
+    nx.draw_networkx(network, edge_color=colors)
+    plt.draw()
+    plt.show()
+
+if __name__ == "__main__":
+    draw_graph()
